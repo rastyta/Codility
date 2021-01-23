cbe9c59977012dbe45719b4eb46910d040f231f8
jose.moreno <jose.moreno@openjawtech.com>
2021-01-23 13:09:09 +0100

    NumberOfDiscIntersections 93%

--- src/main/java/com/rasty/sorting/NumberOfDiscIntersections.java
+++ src/main/java/com/rasty/sorting/NumberOfDiscIntersections.java
@@ -1,48 +1,42 @@
+//https://app.codility.com/demo/results/trainingDYEJW5-G5N/
 package com.rasty.sorting;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class NumberOfDiscIntersections {
   public int solution(int[] A){
     int intersections = 0;
-    List<Circle> circles = new ArrayList<>();
-    for (int i=0; i<A.length; i++) {
-      if(intersections>10000000){
-        return -1;
+    List<Integer> starts = new ArrayList<>();
+    List<Integer> ends = new ArrayList<>();
+
+    for (int i = 0; i < A.length; i++) {
+      starts.add(i-A[i]);
+      ends.add(i+A[i]);
+    }
+    Collections.sort(starts);
+    Collections.sort(ends);
+    //this is the tricky part to understand
+    //on the left we have the boundaries of open circles, on the right the boundary of the next circle
+    //as long as there are circles open (rightLimit[i] >= leftLimit[j]) and (j < A.length)
+    int j = 0;
+    for (int i = 0; i < A.length; i++) {
+      while(j < A.length && ends.get(i) >= starts.get(j)){
+        //we have circles at each position, so, as long as the left boundaries are smaller or equal to the right boundary,
+        // there are circles intersecting there
+        //if j surpasses j, it means we overcalculated and we start to decrease the number of intersections
+        intersections += j-i;
+        //pass to the next open circle
+        j++;
       }
-      int start = i - A[i];
-      int end = i + A[i];
-      for(Circle circle : circles){
-        if(end < circle.start || start>circle.end){
-          continue;
-        }
-        intersections+=1;
-      }
-      circles.add(new Circle(start,end));
+      if(intersections > 10000000) return -1;
     }
     return intersections;
   }
 
   public static void main(String[] args) {
-    int[] A = {1, 2147483647, 0};
+    int[] A = {1, 5, 2, 1, 4, 0};
     System.out.println(new NumberOfDiscIntersections().solution(A));
-  }
-
-  class Circle{
-
-    private int start = 0;
-    private int end = 0;
-
-    public Circle(int start, int end) {
-      this.start = start;
-      this.end = end;
-    }
-    public int getStart() {
-      return start;
-    }
-    public int getEnd() {
-      return end;
-    }
   }
 }
